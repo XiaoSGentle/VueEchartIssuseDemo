2aff4f8c739bfe20ec2080cccd02e32775a5c09c
宋超阳 <393340139@qq.com>
2024-11-28 15:01:45 +0800

    change index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,53 +1,72 @@
 <script setup lang="ts">
+import { BarChart, LineChart } from 'echarts/charts'
+import { GridComponent, LegendComponent, TitleComponent, TooltipComponent } from 'echarts/components'
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { provide, ref } from 'vue'
+import VChart, { THEME_KEY } from 'vue-echarts'
+
 defineOptions({
   name: 'IndexPage',
 })
-const user = useUserStore()
-const name = ref(user.savedName)
+use([CanvasRenderer, LineChart, TitleComponent, TooltipComponent, LegendComponent, GridComponent, BarChart])
+provide(THEME_KEY, 'light')
+const sheetOption = ref({
+  legend: {
+    selectedMode: false,
+  },
+  xAxis: {
+    type: 'category',
+    data: ['11.02', '11.03', '11.04'],
+  },
+  tooltip: {
+    trigger: 'axis',
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [{
+    name: '高压',
+    data: [100, 299, 399],
+    type: 'line',
+    areaStyle: {
+      color: '#71bbff',
+    },
+    smooth: true,
+  }, {
+    name: '低压',
+    data: [10, 29, 39],
+    type: 'line',
+    areaStyle: {
+      color: '#fcff64',
+    },
+    smooth: true,
+  }],
+})
 
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
+function changeTo() {
+  sheetOption.value.series.length = 0
+
+  sheetOption.value.series = [{
+    name: '心率',
+    data: [69, 2, 5],
+    type: 'line',
+    areaStyle: {
+      color: '#fcff64',
+    },
+    smooth: true,
+  }]
 }
-
-const { t } = useI18n()
 </script>
 
 <template>
-  <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        m-3 text-sm btn
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
-    </div>
+  <button @click="changeTo" />
+  <div class="h50vh flex justify-center">
+    <VChart class="h50vh" :option="sheetOption" />
   </div>
+  <button @click="changeTo">
+    点我改变
+  </button>
 </template>
 
 <route lang="yaml">
